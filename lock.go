b9ff000c8944fa17f8249c58c59bfde616e3d6ef
Nickqiaoo <nickqiaoo@gmail.com>
2020-03-17 00:02:00 +0800

    finish redislock

--- lock.go
+++ lock.go
@@ -2,6 +2,6 @@ package distributedlock
 
 // Lock is a distributedlock
 type Lock interface{
-	lock()
-	unlock()
+	Lock()
+	UnLock()
 }
--- redis/redis_lock.go
+++ redis/redis_lock.go
@@ -2,25 +2,93 @@ package redislock
 
 import (
 	"DistirbutedLock/conf"
+	"log"
+	"time"
+
 	"github.com/gomodule/redigo/redis"
 )
 
 //RedisLock impl DistributedLock
 type RedisLock struct {
-	redis *redis.Pool
-	name string
+	redis   *redis.Pool
+	name    string
+	timeout int64
+	stop    chan bool
+	get     chan bool
 }
 
-func (l *RedisLock) lock() {
+//Lock get lock
+func (l *RedisLock) Lock() <-chan bool {
 	conn := l.redis.Get()
 	defer conn.Close()
-	if res, err := redis.Strings(conn.Do("MGET", args...)); err != nil {
-		log.Errorf("conn.Do(MGET %v) error(%v)", args, err)
+	var res int64
+	var err error
+	if res, err = redis.Int64(conn.Do("SETNX", l.name, time.Now().Unix()+l.timeout)); err != nil {
+		log.Println(res)
+	}
+	if res == 1 {
+		go l.expire()
+		l.get <- true
+	} else if res == 0 {
+		go l.check()
+	}
+	return l.get
+}
+
+//UnLock release lock
+func (l *RedisLock) UnLock() {
+	l.stop <- true
+}
+
+func (l *RedisLock) check() {
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		select {
+		case t := <-ticker.C:
+			conn := l.redis.Get()
+			defer conn.Close()
+			var res int64
+			var err error
+			if res, err = redis.Int64(conn.Do("GET", l.name)); err != nil {
+				log.Println(res)
+			}
+			if err == redis.ErrNil || res < time.Now().Unix(){
+				if res, err = redis.Int64(conn.Do("GETSET", l.name, time.Now().Unix()+l.timeout)); err != nil {
+					log.Println(res)
+				}
+				if err == redis.ErrNil || res < time.Now().Unix(){
+					l.get <- true
+					return
+				}
+			}
+			log.Println("Current time: ", t)
+		}
 	}
 }
 
-func (l *RedisLock) unlock() {
-
+func (l *RedisLock) expire() {
+	ticker := time.NewTicker(time.Duration(l.timeout/2) * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-l.stop:
+			log.Println("UnLock")
+			conn := l.redis.Get()
+			defer conn.Close()
+			if res, err := conn.Do("DEL", l.name); err != nil {
+				log.Println(res)
+			}
+			return
+		case t := <-ticker.C:
+			conn := l.redis.Get()
+			defer conn.Close()
+			if res, err := conn.Do("SET", l.name, time.Now().Unix() + l.timeout); err != nil {
+				log.Println(res)
+			}
+			log.Println("Current time: ", t)
+		}
+	}
 }
 
 func newRedisLock(c *conf.Redis) *RedisLock {
